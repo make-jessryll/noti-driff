53f415023ecf52cef3853e7e86480b29b023097e
Jessryll Quitoriano <jessryll@makedevelopment.com>
2019-12-15 10:27:02 +0800

    Update NotificationPage.js

--- src/components/NotificationPage.js
+++ src/components/NotificationPage.js
@@ -3,19 +3,19 @@ import axios from "axios";
 import NotificationList from "./notifications/NotificationList";
 
 class NotificationPage extends React.Component {
-    
+
     constructor() {
         super();
         this.state = {
             skip: 0,
             limit: 20,
-            notifications : []
+            notifications: []
         }
     }
 
     componentDidMount() {
         if (this.state.notifications.length === 0) {
-            
+
             this.requestGetNotifications().then(res => {
                 if (res.status === true) {
                     return this.setState({
@@ -46,25 +46,38 @@ class NotificationPage extends React.Component {
                 'Authorization': `${token_type} ${access_token}`
             }
         })
-        .then(res => {
-            return {
-                status: true,
-                data: res.data
-            }
-        })
-        .catch(err => {
-            return {
-                status: false,
-                data: err
-            }
-        })
-    }
+            .then(res => {
+                return {
+                    status: true,
+                    data: res.data
+                }
+            })
+            .catch(err => {
+                return {
+                    status: false,
+                    data: err
+                }
+            })
+    };
+
+    handleChangeDriff = () => {
+      window.localStorage.clear();
+      window.location.reload();
+    };
 
     render() {
         return (
             <div>
-                <h1>Notification</h1>
-                <NotificationList 
+                <h1>
+                    Notification
+                    <button
+                        className={'btn btn-sm float-right btn-primary'}
+                        onClick={this.handleChangeDriff}
+                    >
+                        Change Driff
+                    </button>
+                </h1>
+                <NotificationList
                     notifications={this.state.notifications}
                 />
             </div>
